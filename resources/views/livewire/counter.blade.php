2f4956f39855d613aa09f8ef715570c82dfa3e6c
subhashmagharola <subhashmagharola@gmail.com>
2025-12-02 14:21:07 +0530

    see how render works

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,7 @@
 <div>
     <h1>{{ $count }}</h1>
  
+    <p style="color:blue; font-style:bold">{{ rand(100,999) }}</p>
     <button wire:click="increment">+</button>
  
     <button wire:click="decrement">-</button>
